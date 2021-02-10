e17762f8ba0dd5e5359fcb204cd8819338438768
Ethan Kowalchuk <ethan.kowalchuk@ontariotechu.net>
2021-02-10 16:59:10 -0500

    Added Midterm Project
    Copied over Week 4 Starter for Midterm

---
+++ OTTER/projects/CG MIDTERM/src/Graphics/Post/ColourCorrectionEffect.cpp
@@ -0,0 +1,50 @@
+#include "ColourCorrectionEffect.h"
+
+void ColourCorrectionEffect::Init(unsigned width, unsigned height)
+{
+    int index = int(_buffers.size());
+    _buffers.push_back(new Framebuffer());
+    _buffers[index]->AddColorTarget(GL_RGBA8);
+    _buffers[index]->AddDepthTarget();
+    _buffers[index]->Init(width, height);
+
+    index = int(_shaders.size());
+    _shaders.push_back(Shader::Create());
+    _shaders[index]->LoadShaderPartFromFile("shaders/passthrough_vert.glsl", GL_VERTEX_SHADER);
+    _shaders[index]->LoadShaderPartFromFile("shaders/Post/colour_correction_frag.glsl", GL_FRAGMENT_SHADER);
+    _shaders[index]->Link();
+}
+
+void ColourCorrectionEffect::ApplyEffect(PostEffect* buffer)
+{
+    BindShader(0);
+    _shaders[0]->SetUniform("u_Intensity", _intensity);
+
+    buffer->BindColourAsTexture(0, 0, 0);
+
+    _buffers[0]->RenderToFSQ();
+
+    buffer->UnbindTexture(0);
+
+    UnbindShader();
+}
+
+void ColourCorrectionEffect::DrawToScreen()
+{
+    BindShader(0);
+    _shaders[0]->SetUniform("u_Intensity", _intensity);
+    BindColourAsTexture(0, 0, 0);
+    _buffers[0]->DrawFullscreenQuad();
+    UnbindTexture(0);
+    UnbindShader();
+}
+
+float ColourCorrectionEffect::GetIntensity() const
+{
+    return _intensity;
+}
+
+void ColourCorrectionEffect::SetIntensity(float intensity)
+{
+    _intensity = intensity;
+}
---
+++ OTTER/projects/CG MIDTERM/src/Graphics/Post/SepiaEffect.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Graphics/Post/PostEffect.h"
+
+class SepiaEffect : public PostEffect
+{
+public:
+	void Init(unsigned width, unsigned height) override;
+	void ApplyEffect(PostEffect* buffer) override;
+	float GetIntensity() const;
+	void SetIntensity(float intensity);
+private:
+	float _intensity = 0.7f;
+};
